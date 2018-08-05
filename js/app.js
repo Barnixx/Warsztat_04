3ab79f8bb8103fb882c39b98f8847ed1c131c38e
Barnixx <rb.kuszaj@gmail.com>
2018-08-05 19:54:48 +0200

    Book ajax

--- js/app.js
+++ js/app.js
@@ -1,18 +1,54 @@
 $(function () {
     var booksList = $('#books');
 
-    $.ajax({
-        url: "http://localhost:8282/books",
-        method: "GET"
-    })
-        .done(function (books) {
-            console.log("dane pobrano z sukcesem");
-            console.log(books);
+    var loadBooks = function () {
+        $.ajax({
+            url: "http://localhost:8282/books",
+            method: "GET"
+        })
+            .done(function (books) {
+                console.log("dane pobrano z sukcesem");
+                console.log(books);
+                booksList.html("");
+                books.forEach(book => {
+                    $("<p data-id='" + book.id + "'>" + book.title + "<button class='delBtn btn btn-danger'>Usuń</button></p><div></div>").appendTo(booksList);
+                })
+            });
+    };
+    loadBooks();
 
-            books.forEach(book => {
-                $("<p data-id='" + book.id + "'>" + book.title + "</p><div></div>").appendTo(booksList);
+    var addBook = function (e) {
+
+        e.preventDefault();
+
+        $.ajax({
+            url: "http://localhost:8282/books",
+            method: "POST",
+            contentType: 'application/json',
+            data:
+                '{"isbn":"' + $("#isbn").val() + '", ' +
+                '"title":"' + $("#title").val() + '", ' +
+                '"publisher":"' + $("#publisher").val() + '",' +
+                '"type":"' + $("#type").val() + '",' +
+                '"author":"' + $("#author").val() + '"}',
+        })
+            .done(function () {
+                console.log("powiodło się");
+
+                loadBooks();
             })
-        });
+            .fail(function () {
+                console.log("niestety coś zawiodło");
+                console.log('{"isbn":"' + $("#isbn").val() + '", ' +
+                    '"title":"' + $("#title").val() + '", ' +
+                    '"publisher":"' + $("#publisher").val() + '",' +
+                    '"type":"' + $("#type").val() + '",' +
+                    '"author":"' + $("#author").val() + '"}');
+            })
+    };
+
+    $('#addBookBtn').on('click', addBook);
+
 
     booksList.on('click', 'p', function () {
 
@@ -24,5 +60,21 @@ $(function () {
             .done(function (book) {
                 bookInfo.text(book.id + " " + book.isbn + " " + book.title + " " + book.author + " " + book.publisher + " " + book.type);
             })
+    })
+    booksList.on('click', 'button.delBtn', function (e) {
+        var bookId = $(this).parent('p').data('id');
+        console.log(bookId);
+        e.preventDefault();
+        console.log("http://localhost:8282/books/" + bookId);
+
+        $.ajax({
+            url: "http://localhost:8282/books/" + bookId,
+            method: "DELETE"
+        })
+            .done(function () {
+                loadBooks();
+            })
+
+
     })
 });
